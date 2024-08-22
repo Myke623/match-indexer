1ee7e446259e9b9bea5cb315097cb16328ef0e4a
Myke623 <mike.abdow@gmail.com>
2024-08-23 00:09:51 +1000

    Added tekken 8 layout and templates

---
+++ layouts/tekken8.py
@@ -0,0 +1,11 @@
+# General
+layout = {
+    "scale" : 1.0, # Normalised to fullscreen
+    "originPlayer1" : (50,76),
+    "originPlayer2" : (1125,76),
+    "widthPortrait" : 105,
+    "heightPortrait" : 23,
+    "originClock" : (0,0), # Not used
+    "widthClock" : 1, # Not used
+    "heightClock" : 1 # Not used
+}
