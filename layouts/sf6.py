74defdbdbb8662438aa7ce1c48bce2dd3b01eb9e
Myke623 <mike.abdow@gmail.com>
2024-09-03 00:50:12 +1000

    Added sf6 layout and templates

---
+++ layouts/sf6.py
@@ -0,0 +1,9 @@
+# General
+layout = {
+    "scale" : 1.0, # Normalised to fullscreen
+    "originPlayer1" : (0,12),
+    "originPlayer2" : (1190,12),
+    "widthPortrait" : 90,
+    "heightPortrait" : 26,
+    "threshold" : 0.9 # Detection threshold (%)
+}
