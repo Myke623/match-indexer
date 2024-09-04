e448a239d1167f64d5a8d3fc1ac5ce99fe5746a5
Myke623 <mike.abdow@gmail.com>
2024-09-05 01:15:34 +1000

    Added VF5US/VFes layout and templates

---
+++ layouts/vf5us.py
@@ -0,0 +1,9 @@
+# Virtua Fighter 5 Ultimate Showdown (VF5US) / Virtua Fighter esports (VFes)
+layout = {
+    "scale" : 1.0, # Normalised to fullscreen
+    "originPlayer1" : (27,89),
+    "originPlayer2" : (1198,89),
+    "widthPortrait" : 54,
+    "heightPortrait" : 24,
+    "threshold" : 0.9 # Detection threshold (%)
+}
